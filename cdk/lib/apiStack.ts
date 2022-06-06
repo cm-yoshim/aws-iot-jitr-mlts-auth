8829f09b4df4fb24ef5fc2fbaf8ceffcdab60d1a
cm-yoshim <yoshimura.keita@classmethod.jp>
2022-06-06 19:23:46 +0900

    不要処理を削除

--- cdk/lib/apiStack.ts
+++ cdk/lib/apiStack.ts
@@ -76,31 +76,3 @@ export class ApiStack extends cdk.Stack {
     });
   }
 }
-
-export const addCorsOptions = (apiResource: apig.IResource) => {
-  apiResource.addMethod('OPTIONS', new apig.MockIntegration({
-    integrationResponses: [{
-      statusCode: '200',
-      responseParameters: {
-        'method.response.header.Access-Control-Allow-Headers': "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Amz-User-Agent'",
-        'method.response.header.Access-Control-Allow-Origin': "'*'",
-        'method.response.header.Access-Control-Allow-Credentials': "'false'",
-        'method.response.header.Access-Control-Allow-Methods': "'OPTIONS,GET,PUT,POST,DELETE'",
-      },
-    }],
-    passthroughBehavior: apig.PassthroughBehavior.NEVER,
-    requestTemplates: {
-      "application/json": "{\"statusCode\": 200}"
-    },
-  }), {
-    methodResponses: [{
-      statusCode: '200',
-      responseParameters: {
-        'method.response.header.Access-Control-Allow-Headers': true,
-        'method.response.header.Access-Control-Allow-Methods': true,
-        'method.response.header.Access-Control-Allow-Credentials': true,
-        'method.response.header.Access-Control-Allow-Origin': true,
-      },
-    }]
-  })
-}
